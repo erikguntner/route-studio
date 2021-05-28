ec3558418a8c4542f5a2a2a9dcc4938aec0b8fe4
Erik <eguntner2291@gmail.com>
2021-05-28 16:42:22 -0700

    Replace old styles with Styled Components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import {
   createGlobalStyle,
@@ -6,7 +5,25 @@ import {
   DefaultTheme,
 } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle``;
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+    position: relative;
+  }
+`;
 
 const theme: DefaultTheme = {
   colors: {
