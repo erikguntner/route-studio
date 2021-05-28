7181ca37cc5e7bdd165ebaa2c164fdfb4137a025
Erik <eguntner2291@gmail.com>
2021-05-28 16:35:33 -0700

    Create custom _document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,27 @@
 import '../styles/globals.css';
 import type {AppProps} from 'next/app';
+import {
+  createGlobalStyle,
+  ThemeProvider,
+  DefaultTheme,
+} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle``;
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
 
 function MyApp({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 export default MyApp;
