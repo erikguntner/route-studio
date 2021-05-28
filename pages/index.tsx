0a520c0a9f5f163cc74316ed2288c4ccb81863e9
Erik <eguntner2291@gmail.com>
2021-05-28 15:15:16 -0700

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { NextApiHandler } from 'next';
 
 export default function Home() {
   return (
