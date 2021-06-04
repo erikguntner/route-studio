cc858602ed7fd25fd955a263238fcf7e3171f635
Erik <eguntner2291@gmail.com>
2021-06-04 16:41:20 -0700

    Update for production

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -91,7 +91,7 @@ const options: NextAuthOptions = {
     Providers.Email({
       server: {
         host: process.env.EMAIL_SERVER_HOST || '',
-        port: 587,
+        port: process.env.NODE_ENV === 'production' ? 465 : 587,
         auth: {
           user: process.env.EMAIL_SERVER_USER || '',
           pass: process.env.EMAIL_SERVER_PASSWORD || '',
