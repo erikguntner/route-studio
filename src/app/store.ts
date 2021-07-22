36072de843c653a4b12fc5db56694d4fa00fb405
Erik <eguntner2291@gmail.com>
2021-07-22 16:33:46 -0700

    Add createAsyncThunk example and tests to counter

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,23 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {
+  configureStore,
+  combineReducers,
+  ThunkAction,
+  Action,
+} from '@reduxjs/toolkit';
 import counterReducer from '../features/Counter/counterSlice';
 
+const rootReducer = combineReducers({counter: counterReducer});
+
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: rootReducer,
+  devTools: process.env.NODE_ENV !== 'production',
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+export type RootState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
