a084794ed69c7f9541409d9e774476bf3b0dfc5a
Erik <eguntner2291@gmail.com>
2021-07-30 16:30:27 -0700

    Add ccheck for removing point after creation

--- cypress/integration/create-page.spec.ts
+++ cypress/integration/create-page.spec.ts
@@ -1,14 +1,17 @@
 describe('Create Page', () => {
-  it('Should create point when selecting a destination', () => {
+  it('Should create point when selecting a searched destination', () => {
     cy.visit('/create');
 
+    // checks buttons should be disabled
+    cy.findByRole('button', {name: 'redo'}).should('be.disabled');
+    cy.findByRole('button', {name: 'undo'}).should('be.disabled');
+    cy.findByRole('button', {name: 'clear'}).should('be.disabled');
+
     // Type into search box
-    cy.findByRole('combobox', {name: /locations/i}).type('Claremont');
+    cy.findByRole('combobox', {name: /locations/i}).type('claremont');
     cy.findByRole('listbox').within(() => {
       cy.findByText(/no results/i).should('not.exist');
-      cy.findAllByRole('option', {name: /Claremont/i})
-        .first()
-        .click();
+      cy.findAllByRole('option').first().click();
     });
 
     cy.findByRole('listbox').should('not.exist');
@@ -16,6 +19,13 @@ describe('Create Page', () => {
     cy.findByRole('main').within(() => {
       cy.findByRole('button', {name: /add point/i}).click();
       cy.findAllByTestId('point').should('have.length', 1);
+
+      cy.findByRole('button', {name: 'redo'}).should('be.disabled');
+      cy.findByRole('button', {name: 'undo'}).should('not.be.disabled');
+      cy.findByRole('button', {name: 'clear'}).should('not.be.disabled');
+
+      cy.findByRole('button', {name: 'clear'}).click();
+      cy.findAllByTestId('point').should('have.length', 0);
     });
   });
 });
