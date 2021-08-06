08cf348e7e1a9f802f07b9c505e6103c104cd730
Erik <eguntner2291@gmail.com>
2021-08-06 00:03:04 -0700

    Create login test and extend Session type

---
+++ types/next-auth.d.ts
@@ -0,0 +1,17 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import NextAuth from 'next-auth';
+
+declare module 'next-auth' {
+  /**
+   * Returned by `useSession`, `getSession` and received as a prop on the `Provider` React Context
+   */
+  interface Session {
+    user?: {
+      name?: string | null;
+      email?: string | null;
+      image?: string | null;
+      id?: string | null;
+    };
+    expires?: string;
+  }
+}
